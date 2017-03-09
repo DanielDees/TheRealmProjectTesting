f62c4336fc197e5536658d3e9773401a3f6ac398
DanielDees <explorers2010@hotmail.com>
2017-03-09 01:01:26 -0500

    Added heavy documentation to game_Maps file

--- scripts/game_Maps.js
+++ scripts/game_Maps.js
@@ -21,7 +21,9 @@ function Game_map_loader() {
   //Load all tiles within this range  of player
   this.renderRange = 10 * this.tileSize;
 
-  this.resetMap = function() {
+  //Clears all map, enemy, projectile, loot, and portal data.
+  //Resets player to default X/Y
+  this.reset = function() {
 
     lootBagList = [];
     portalList = [];
@@ -35,121 +37,108 @@ function Game_map_loader() {
     REALM_MAP = [];
     BOSS_ROOM_MAP = [];
   };
+  //Generates the game REALM maps
   this.generateGameMap = function() {
 
     //Reset everything. Place players in realm.
-    this.resetMap();
+    this.reset();
+
+    //Number of enemies that will spawn
     enemies_remaining_in_realm = 100;
 
     //Creates the map grid.
     for (var row = 0; row < this.MAX_SIZE; row++) {
       for (var col = 0; col < this.MAX_SIZE; col++) {
 
-        var chance = Math.random();
-
+        //Tile Data
         var data = {
 
           //Location
-          X: col * this.tileSize,
-          Y: row * this.tileSize,
+          X: this.tileSize * col,
+          Y: this.tileSize * row,
 
           //Info
           img: waterGround[0],
           mm_color: "white",
-          elevation: 10,
         }
 
-        //Low elevation creates water.
-        //if (elevation < 1) { REALM_MAP.push([waterGround[0] , realCol * this.tileSize, realRow * this.tileSize, elevation]); }
-        if (chance <= 0.7) { 
+        //RNG 0 - 1
+        var chance = Math.random();
 
-          data.img = grassGround[0];
-          data.mm_color = 'rgb(' + 3 + ',' + 126 + ',' + 31 + ')';
-          REALM_MAP.push(data); 
-        }
-        else if (chance <= 0.8) { 
+        //Get tile sprite
+             if (chance <= 0.7) { data.img = grassGround[0]; }
+        else if (chance <= 0.8) { data.img = grassGround[1]; }
+        else if (chance <= 0.9) { data.img = grassGround[2]; }
+        else if (chance <= 1.0) { data.img = grassGround[3]; };
 
-          data.img = grassGround[1];
-          data.mm_color = 'rgb(' + 3 + ',' + 126 + ',' + 31 + ')';
-          REALM_MAP.push(data); 
-        }
-        else if (chance <= 0.9) { 
+        //Mini-map color is the same for all grassGround tiles
+        data.mm_color = 'rgb(' + 3 + ',' + 126 + ',' + 31 + ')';
 
-          data.img = grassGround[2];
-          data.mm_color = 'rgb(' + 3 + ',' + 126 + ',' + 31 + ')';
-          REALM_MAP.push(data); 
-        }
-        else if (chance <= 1) { 
-
-          data.img = grassGround[3];
-          data.mm_color = 'rgb(' + 3 + ',' + 126 + ',' + 31 + ')';
-          REALM_MAP.push(data); 
-        };
+        //Add tile to map
+        REALM_MAP.push(data); 
       };
     };
 
+    //Use REALM_MAP for rendering
     MAP_TYPE = REALM_MAP;
+
+    //Get tile colors for mini-map
     map_tile_color_loader.load();
   };
+  //Generates the BOSS map
   this.generateBossMap = function() {
 
     //Reset everything. Place players in boss room.
-    this.resetMap();
+    this.reset();
 
     //Creates the map grid.
     for (var row = 0; row < this.MAX_SIZE; row++) {
       for (var col = 0; col < this.MAX_SIZE; col++) {
 
-        var chance = Math.random();
-
+        //Tile Data
         var data = {
 
           //Location
-          X: col * this.tileSize,
-          Y: row * this.tileSize,
+          X: this.tileSize * col,
+          Y: this.tileSize * row,
 
           //Info
           img: waterGround[0],
           mm_color: "white",
-          elevation: 10,
         }
+
+        //RNG 0 - 1
+        var chance = Math.random();
         
-        if (chance <= 0.7) { 
+        //Get tile sprite
+             if (chance <= 0.7) { data.img = stoneGround[0]; }
+        else if (chance <= 0.8) { data.img = stoneGround[1]; }
+        else if (chance <= 0.9) { data.img = stoneGround[2]; }
+        else if (chance <= 1.0) { data.img = stoneGround[3]; };
 
-          data.img = stoneGround[0];
-          data.mm_color = 'rgb(' + 167 + ',' + 185 + ',' + 185 + ')';
-          BOSS_ROOM_MAP.push(data); 
-        }
-        else if (chance <= 0.8) { 
+        //Mini-map color is the same for all grassGround tiles
+        data.mm_color = 'rgb(' + 167 + ',' + 185 + ',' + 185 + ')';
 
-          data.img = stoneGround[1];
-          data.mm_color = 'rgb(' + 167 + ',' + 185 + ',' + 185 + ')';
-          BOSS_ROOM_MAP.push(data); 
-        }
-        else if (chance <= 0.9) { 
-
-          data.img = stoneGround[2];
-          data.mm_color = 'rgb(' + 167 + ',' + 185 + ',' + 185 + ')';
-          BOSS_ROOM_MAP.push(data); 
-        }
-        else if (chance <= 1){ 
-
-          data.img = stoneGround[3];
-          data.mm_color = 'rgb(' + 167 + ',' + 185 + ',' + 185 + ')';
-          BOSS_ROOM_MAP.push(data); 
-        };
+        //Add tile to map
+        BOSS_ROOM_MAP.push(data); 
       };
     };
 
+    //Spawn Boss
     spawn_Game_Boss();
 
+    //Use BOSS_ROOM_MAP for rendering
     MAP_TYPE = BOSS_ROOM_MAP;
+
+    //Get tile colors for mini-map
     map_tile_color_loader.load();
   };
+  //Un-used for now, may make dungeon generator file later
   this.placeWall = function(row, col) {
-
-    MAP_TILES.push([classSelectionPics[0], col * this.tileSize, row * this.tileSize]);
+    
+    MAP_TILES.push([waterGround[0], col * this.tileSize, row * this.tileSize]);
   };
+  //Gets tile colors for mini-map
   this.getRenderedTiles = function() {
 
     //Holds all loaded tile data
@@ -172,10 +161,9 @@ function Game_map_loader() {
         var tileNum = (i * this.MAX_SIZE) + j;
 
         try { 
-          if (tileNum >= 0 && tileNum < MAP_TYPE.length) { 
+          if (tileNum >= 0 && tileNum < MAP_TYPE.length) {
 
-            //Get tile color for minimap
-            //Add tile in row to row data
+            //Add tile color to row data for minimap
             rowTileData.push(MAP_TILE_COLORS[tileNum]);
           } 
         }
@@ -184,12 +172,14 @@ function Game_map_loader() {
 
       //Add row to the loaded tile list
       tileData.push(rowTileData);
+      //Reset the row
       rowTileData = [];
     };
 
-    //Return all loaded tiles
+    //Return all loaded tile colors
     return tileData;
   };
+  //Renders all tiles within range of the player
   this.draw = function() {
 
     //Get location for map rendering
@@ -203,12 +193,13 @@ function Game_map_loader() {
       //Loop through left most to right most tiles rendered
       for (var j = minX; j <= maxX; j++) { 
 
-        //i = the row (each row being this.MAX_SIZE in length)
-        //j = tiles over that you are.
+        //i * this.MAX_SIZE = the row
+        //j = column of row
         var k = (i * this.MAX_SIZE) + j;
 
         try { 
           if (k >= 0 && k < MAP_TYPE.length) { 
+            //Draw tile images for map
             ctx.drawImage(MAP_TYPE[k].img, MAP_TYPE[k].X, MAP_TYPE[k].Y, 50, 50); 
           } 
         }
